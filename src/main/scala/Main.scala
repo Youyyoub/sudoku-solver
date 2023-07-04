f6f55ff46c269d35ce968d2e06bc651db6e086a0
SOFIANYY <94124501+SOFIANYY@users.noreply.github.com>
2023-07-04 11:11:20 +0200

    first commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,11 +4,33 @@ import zio._
 
 object Main extends ZIOAppDefault {
 
+  
+
   def run: ZIO[Any, Throwable, Unit] =
     for {
       _ <- Console.print("Enter the path to the JSON file containing the Sudoku problem:")
       path <- Console.readLine
       _ <-  Console.printLine(s"You entered: $path")
       // Add your Sudoku solver logic here, utilizing ZIO and interacting with the ZIO Console
+
+      JSON_Sudoku_Data <- openFile(path).catchAll { error =>
+      for {
+        _ <- ZIO.logErrorCause("Could not open primary file", Cause.fail(new Throwable(error)))
+        _ <- Console.print("Enter another or the right path to a JSON file:")
+        backupPath <- Console.readLine
+        file <- openFile(backupPath)
+      } yield file 
+      _ <-  Console.printLine("Here is a nice view of our sudoku")
+      prettyString(JSON_Sudoku_Data)
+      }
+
+
+
+
     } yield ()
+
+    object Sudoku {
+      
+  }
+
 }
