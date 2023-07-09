1165969efd39c375cf3f173602afc673ee416e07
Youyyoub <74156463+Youyyoub@users.noreply.github.com>
2023-07-09 14:17:38 +0200

    Added the "solve" function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,18 +19,18 @@ object Main extends ZIOAppDefault {
   )
 
   def run: ZIO[Any, Throwable, Unit] = {
-
     for {
       // _ <- Console.print("Enter the path to the JSON file containing the Sudoku problem:")
       // path <- Console.readLine
       // _ <-  Console.printLine(s"You entered: $path")
     
-      // _ <- Console.printLine(Sudoku.prettyString(sudokuTestGrid))
-      _ <- Console.printLine(Sudoku.validate(sudokuTestGrid, 5, 2, 4))
-      _ <- Console.printLine(Sudoku.validate(sudokuTestGrid, 5, 2, 5))
-      _ <- Console.printLine(Sudoku.validate(sudokuTestGrid, 5, 2, 7))
-    } yield ()
-
+      // Print the unresolved grid
+      _ <- Console.printLine(Sudoku.prettyString(sudokuTestGrid))
+      
+    } yield {
+      // Resolve the grid
+      Sudoku.solve(sudokuTestGrid)
+    }
   }
 }
 
@@ -62,9 +62,23 @@ object Sudoku {
     }.mkString("+-------+-------+-------+\n", "\n+-------+-------+-------+\n", "\n+-------+-------+-------+")
   }
 
+  /** Function to determine whether a value placed at the (x, y) coordinates
+    * of a sudoku grid validates or invalidates the grid.
+    * 
+    * Input:
+    * @param sudoku the sudoku grid we want to test
+    * @param x the x coordinate at which we want to test our value
+    * @param y the y coordinate at which we want to test our value
+    * @param value the value we want to test
+    * 
+    * Output:
+    *   Boolean indicating whether the value tested at coordinates (x, y) is valid
+    *   - true: the value can be placed at these coordinates
+    *   - false: the value invalidate the sudoku grid
+    */
   def validate(sudoku: Board, x: Int, y: Int, value: Int): Boolean = {
     val row = sudoku(y)
-    val rowPropertiy = !row.contains(value) // Checking if the row already contains the value we're trying to fill
+    val rowPropertiy = !row.contains(value)
 
     val column = sudoku.map(r => r.apply(x))
     val columnProperty = !column.contains(value)
@@ -81,4 +95,25 @@ object Sudoku {
     rowPropertiy && columnProperty && boxProperty
   }
 
+  /**
+    * Function that solves a sudoku grid by testing all possible values using the `validate` function.
+    * 
+    * The function end when:
+    * - we reached the end of the board, i.e. y >= 9, which means we’ve filled the board and we have a solution
+    * - we’ve tested all possible values
+    *
+    * @param sudoku the sudoku grind we want to solve
+    * @param x
+    * @param y
+    */
+  def solve(sudoku: Board, x: Int = 0, y: Int = 0): Unit = {
+    if (y >= 9) println(prettyString(sudoku)) // We reached the final solution
+    else if (x >= 9) solve(sudoku, 0, y + 1)
+    else if (sudoku(y)(x) > 0) solve(sudoku, x + 1, y)
+    else (1 to 9).filter(value => validate(sudoku, x, y, value)).foreach { value =>
+      sudoku(y)(x) = value  // fill the sudoku grid with the value
+      solve(sudoku, x + 1, y)  // try the next cell
+      sudoku(y)(x) = 0  // remove the value
+    }
+  }
 }
