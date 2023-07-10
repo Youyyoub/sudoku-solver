fa1105e02c7fd179ef01e1905129628192cdb0f0
Karam Mansour <karam.mansour@innowide.fr>
2023-07-10 16:56:58 +0200

    unit to option[Board]

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,31 +15,32 @@ object Main extends ZIOAppDefault {
     Array(7, 0, 0, 0, 2, 0, 0, 0, 6),
     Array(0, 6, 0, 0, 0, 0, 2, 8, 0),
     Array(0, 0, 0, 4, 1, 9, 0, 0, 5),
-    Array(0, 0, 0, 0, 8, 0, 0, 7, 9),
+    Array(0, 0, 0, 0, 8, 0, 0, 7, 9)
   )
 
   def run: ZIO[Any, Throwable, Unit] = {
     for {
-      _ <- Console.print("Enter the path to the JSON file containing the Sudoku problem: ")
-      path <- Console.readLine
-      _ <-  Console.printLine(s"You entered: $path")
+      // _ <- Console.print("Enter the path to the JSON file containing the Sudoku problem: ")
+      // path <- Console.readLine
+      // _ <- Console.printLine(s"You entered: $path")
 
-      jsonSudokuData <- Sudoku.openFile(path).catchAll { error =>
-        for {
-          _ <- Console.printLine("Could not open primary file")
-          _ <- Console.printLine("Enter another or the right path to a JSON file:")
-          backupPath <- Console.readLine
-          jsonSudokuData <- Sudoku.openFile(backupPath)
-        } yield jsonSudokuData
-      }
+      // jsonSudokuData <- Sudoku.openFile(path).catchAll { error =>
+      //   for {
+      //     _ <- Console.printLine("Could not open primary file")
+      //     _ <- Console.printLine("Enter another or the right path to a JSON file:")
+      //     backupPath <- Console.readLine
+      //     jsonSudokuData <- Sudoku.openFile(backupPath)
+      //   } yield jsonSudokuData
+      // }
 
       _ <- Console.printLine("Loaded Sudoku: ")
-      // _ <- Console.printLine(Sudoku.prettyString(jsonSudokuData) + "\n")
+      _ <- Sudoku.solve(sudokuTestGrid) match {
+        case Some(solution) =>
+          Console.printLine(Sudoku.prettyString(solution) + "\n")
+        case None => Console.printLine("No solution found.\n")
+      }
 
-    } yield {
-      // Resolve the grid
-      // Sudoku.solve(sudokuTestGrid)
-    }
+    } yield ()
   }
 }
 
@@ -48,70 +49,49 @@ object Sudoku {
   type Board = Array[Array[Int]]
 
   case class SudokuGrid(
-    row1: String,
-    row2: String,
-    row3: String,
-    row4: String,
-    row5: String,
-    row6: String,
-    row7: String,
-    row8: String,
-    row9: String,
+      row1: String,
+      row2: String,
+      row3: String,
+      row4: String,
+      row5: String,
+      row6: String,
+      row7: String,
+      row8: String,
+      row9: String
   )
 
-  implicit val SudokuGridDecoder: JsonDecoder[SudokuGrid] = DeriveJsonDecoder.gen[SudokuGrid]
+  implicit val SudokuGridDecoder: JsonDecoder[SudokuGrid] =
+    DeriveJsonDecoder.gen[SudokuGrid]
 
   def openFile(path: String): ZIO[Any, Throwable, Array[String]] =
     ZIO.succeed(Source.fromFile(path).getLines.toArray)
-  
+
   def convertStringArray2IntArray(jsonSudoku: Array[String]): Board = {
     return null
   }
 
-  /** Function used to display a soduku in the console in an attractive way.
-    * 
-    * Input:
-    * @param sudoku the sudoku grid we want to display
-    * 
-    * Example Output:
-    * +-------+-------+-------+
-    * | 5 3 4 | 6 7 8 | 9 1 2 |
-    * | 6 7 2 | 1 9 5 | 3 4 8 |
-    * | 1 9 8 | 3 4 2 | 5 6 7 |
-    * +-------+-------+-------+
-    * | 8 5 9 | 7 6 1 | 4 2 3 |
-    * | 4 2 6 | 8 5 3 | 7 9 1 |
-    * | 7 1 3 | 9 2 4 | 8 5 6 |
-    * +-------+-------+-------+
-    * | 9 6 1 | 5 3 7 | 2 8 4 |
-    * | 2 8 7 | 4 1 9 | 6 3 5 |
-    * | 3 4 5 | 2 8 6 | 1 7 9 |
-    * +-------+-------+-------+
-    */
   def prettyString(sudoku: Board): String = {
-    sudoku.grouped(3).map { bigGroup =>
-      bigGroup.map { row =>
-        row.grouped(3).map { smallGroup =>
-          smallGroup.mkString(" ", " ", " ")
-        }.mkString("|", "|", "|")
-      }.mkString("\n")
-    }.mkString("+-------+-------+-------+\n", "\n+-------+-------+-------+\n", "\n+-------+-------+-------+")
+    sudoku
+      .grouped(3)
+      .map { bigGroup =>
+        bigGroup
+          .map { row =>
+            row
+              .grouped(3)
+              .map { smallGroup =>
+                smallGroup.mkString(" ", " ", " ")
+              }
+              .mkString("|", "|", "|")
+          }
+          .mkString("\n")
+      }
+      .mkString(
+        "+-------+-------+-------+\n",
+        "\n+-------+-------+-------+\n",
+        "\n+-------+-------+-------+"
+      )
   }
 
-  /** Function to determine whether a value placed at the (x, y) coordinates
-    * of a sudoku grid validates or invalidates the grid.
-    * 
-    * Input:
-    * @param sudoku the sudoku grid we want to test
-    * @param x the x coordinate at which we want to test our value
-    * @param y the y coordinate at which we want to test our value
-    * @param value the value we want to test
-    * 
-    * Output:
-    *   Boolean indicating whether the value tested at coordinates (x, y) is valid
-    *   - true: the value can be placed at these coordinates
-    *   - false: the value invalidate the sudoku grid
-    */
   def validate(sudoku: Board, x: Int, y: Int, value: Int): Boolean = {
     val row = sudoku(y)
     val rowPropertiy = !row.contains(value)
@@ -122,8 +102,8 @@ object Sudoku {
     val boxX = x / 3
     val boxY = y / 3
     val box = for {
-      yb <- (boxY * 3) until(boxY * 3 + 3)
-      xb <- (boxX * 3) until(boxX * 3 + 3)
+      yb <- (boxY * 3) until (boxY * 3 + 3)
+      xb <- (boxX * 3) until (boxX * 3 + 3)
     } yield sudoku(yb)(xb)
 
     val boxProperty = !box.contains(value)
@@ -131,25 +111,39 @@ object Sudoku {
     rowPropertiy && columnProperty && boxProperty
   }
 
-  /**
-    * Function that solves a sudoku grid by testing all possible values using the `validate` function.
-    * 
-    * The function end when:
-    * - we reached the end of the board, i.e. y >= 9, which means we’ve filled the board and we have a solution
-    * - we’ve tested all possible values
-    *
-    * @param sudoku the sudoku grind we want to solve
-    * @param x
-    * @param y
-    */
-  def solve(sudoku: Board, x: Int = 0, y: Int = 0): Unit = {
-    if (y >= 9) println(prettyString(sudoku)) // We reached the final solution
-    else if (x >= 9) solve(sudoku, 0, y + 1)
-    else if (sudoku(y)(x) > 0) solve(sudoku, x + 1, y)
-    else (1 to 9).filter(value => validate(sudoku, x, y, value)).foreach { value =>
-      sudoku(y)(x) = value  // fill the sudoku grid with the value
-      solve(sudoku, x + 1, y)  // try the next cell
-      sudoku(y)(x) = 0  // remove the value
+  def solve(sudoku: Board, x: Int = 0, y: Int = 0): Option[Board] = {
+    if (y == 9) {
+      return Some(sudoku.map(_.clone)) // We reached the final solution
+    }
+
+    if (sudoku(y)(x) != 0) {
+      nextCell(sudoku, x, y)
+    } else {
+      (1 to 9).foldLeft[Option[Board]](None) { (acc, value) =>
+        if (acc.isDefined) {
+          acc
+        } else {
+          if (validate(sudoku, x, y, value)) {
+            sudoku(y)(x) = value
+            val solved = nextCell(sudoku, x, y)
+            if (solved.isDefined) {
+              solved
+            } else {
+              sudoku(y)(x) = 0
+              None
+            }
+          } else None
+        }
+      }
     }
   }
+
+  def nextCell(sudoku: Board, x: Int, y: Int): Option[Board] = {
+    if (x == 8) {
+      solve(sudoku, 0, y + 1)
+    } else {
+      solve(sudoku, x + 1, y)
+    }
+  }
+
 }
