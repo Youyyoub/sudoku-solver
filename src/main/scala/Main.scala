1759a2949aed500c426f0017659286da0865e2f5
Youyyoub <74156463+Youyyoub@users.noreply.github.com>
2023-07-10 14:44:31 +0200

    Added some docstrings and test grids

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -61,6 +61,26 @@ object Sudoku {
 
   implicit val SudokuGridDecoder: JsonDecoder[SudokuGrid] = DeriveJsonDecoder.gen[SudokuGrid]
 
+  /** Function used to display a soduku in the console in an attractive way.
+    * 
+    * Input:
+    * @param sudoku the sudoku grid we want to display
+    * 
+    * Example Output:
+    * +-------+-------+-------+
+    * | 5 3 4 | 6 7 8 | 9 1 2 |
+    * | 6 7 2 | 1 9 5 | 3 4 8 |
+    * | 1 9 8 | 3 4 2 | 5 6 7 |
+    * +-------+-------+-------+
+    * | 8 5 9 | 7 6 1 | 4 2 3 |
+    * | 4 2 6 | 8 5 3 | 7 9 1 |
+    * | 7 1 3 | 9 2 4 | 8 5 6 |
+    * +-------+-------+-------+
+    * | 9 6 1 | 5 3 7 | 2 8 4 |
+    * | 2 8 7 | 4 1 9 | 6 3 5 |
+    * | 3 4 5 | 2 8 6 | 1 7 9 |
+    * +-------+-------+-------+
+    */
   def prettyString(sudoku: Board): String = {
     sudoku.grouped(3).map { bigGroup =>
       bigGroup.map { row =>
